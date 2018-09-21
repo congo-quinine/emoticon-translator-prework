a536ee6533ab190f423bcdd0232e57e8dbc4b71e
Rob Parker <into.the.parker@gmail.com>
2018-09-21 17:27:13 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,12 +13,12 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
- load_library(file_path)["get_emoticon"].each do |key,values|
+  load_library(file_path)["get_emoticon"].each do |key,values|
       if key == emoticon 
         return values
       end 
     end 
-    return "Sorry, that emoticon was not found"
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning
