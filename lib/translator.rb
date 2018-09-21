ce12f48ca7b1a4831987ae31f50505bbc08ef139
Rob Parker <into.the.parker@gmail.com>
2018-09-21 17:21:27 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,8 +16,6 @@ def get_japanese_emoticon(file_path, emoticon)
    load_library(file_path)["get_emoticon"].each do |key,values|
       if key == emoticon 
         return values
-      else 
-        return 'Apology'
       end 
     end 
 end
